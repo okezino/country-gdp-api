4182bb99191fd5c7ae5c2746491c312bab974051
okezino <okezi002@gmail.com>
2025-10-19 01:42:03 +0400

    first commit

---
+++ src/main/kotlin/com/camavinga/okezino/profile_api/ProfileApiApplication.kt
@@ -0,0 +1,11 @@
+package com.camavinga.okezino.profile_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProfileApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProfileApiApplication>(*args)
+}
